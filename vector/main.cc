6a8251002dec1c24cc891f11560deceecb3aad16
Justin Michaud <justin@justinmichaud.com>
2017-12-10 16:27:07 -0500

    .

--- vector/main.cc
+++ vector/main.cc
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <vector>
 
+template<typename T>
+struct MyAllocator {
+    using value_type = T;
+    template<typename U> struct rebind {
+        using other = MyAllocator<U>;
+    };
+
+    T *allocate(size_t n) {
+        std::cout << "Allocate " << n << "\n";
+        return static_cast<T*>(malloc(sizeof(T)*n));
+    }
+
+    void deallocate(T *p, size_t n) {
+        free(p);
+        std::cout << "Deallocate " << p << " of size " << n << "\n";
+    }
+};
+
 class C {
 public:
     int a=0;
@@ -8,7 +26,7 @@ public:
 };
 
 int main() {
-    std::vector<C> v;
+    std::vector<C, MyAllocator<C>> v;
     v.push_back(C{5});
 
     std::cout << v[0].a << '\n';
