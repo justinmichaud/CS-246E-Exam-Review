d04ff35cfd6405dcd01d60d183bc70142749d389
Justin Michaud <justin@justinmichaud.com>
2017-12-10 17:11:19 -0500

    vector base

--- vector/main.cc
+++ vector/main.cc
@@ -2,6 +2,62 @@
 #include <vector>
 
 template<typename T>
+void uninitialized_move_or_copy(T *to, T *from, T *end) {
+    while (from != end) {
+        if (std::is_nothrow_move_constructible<T>::value) {
+            new (to) T{std::move(*from)};
+        } else {
+            new (to) T{*from};
+        }
+        ++from; ++to;
+    }
+}
+
+template<typename T>
+class vector {
+    struct vector_base {
+        size_t cap;
+        T* arr;
+
+        vector_base(size_t cap): cap{cap == 0? 1: cap}, 
+            arr{static_cast<T*>(operator new(sizeof(T)*this->cap))} {}
+        vector_base(const vector_base &o) = delete;
+        vector_base(vector_base &&o): cap{o.cap}, arr{o.arr} { o.cap = 0; o.arr = nullptr; }
+        ~vector_base() { operator delete(arr); cap = 0; }
+        vector_base &operator=(vector_base o) { std::swap(cap, o.cap); std::swap(arr, o.arr); return *this; }
+    };
+
+    vector_base vb;
+    size_t n;
+
+    void increaseCap() {
+        if (n == vb.cap) {
+            vector_base vb2{vb.cap == 0? 1 : vb.cap*2};
+            uninitialized_move_or_copy(vb2.arr, vb.arr, vb.arr+vb.cap);
+            std::swap(vb, vb2);
+        }
+    }
+public:
+    vector(): vb{0}, n{0} {}
+    vector(const vector& o): vb{o.vb}, n{o.n} {}
+    vector(vector &&o): vb{std::move(o.vb)}, n{o.n} { o.n = 0; }
+    vector<T> &operator=(vector o) { std::swap(vb, o.vb); std::swap(n, o.n); return *this; }
+
+    void push_back(T t) {
+        increaseCap();
+        new (vb.arr + (n++)) T{t};
+    }
+
+    void pop_back() {
+        vb.arr[--n].~T();
+    }
+
+    T &operator[](size_t i) {
+        return vb.arr[i];
+    }
+};
+
+template<typename T>
 struct MyAllocator {
     using value_type = T;
     template<typename U> struct rebind {
@@ -26,8 +82,10 @@ public:
 };
 
 int main() {
-    std::vector<C, MyAllocator<C>> v;
+    vector<C>v;//, MyAllocator<C>> v;
     v.push_back(C{5});
+    v.push_back(C{7});
+    v.pop_back();
 
     std::cout << v[0].a << '\n';
 }
